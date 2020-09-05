6a69e6c877921ffe4c23401beebdab716dd05cf8
Teo Orthlieb <inspirateur.pro@gmail.com>
2020-09-05 14:03:48 -0400

    added rain command

--- src/main/java/io/github/Inspirateur/MCQOL/Main.java
+++ src/main/java/io/github/Inspirateur/MCQOL/Main.java
@@ -9,12 +9,15 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
+
 public class Main extends JavaPlugin implements Plugin, Listener {
 	private Pacifists pacifists;
+	private boolean rain = true;
 
 	@Override
 	public void onEnable() {
@@ -28,6 +31,13 @@ public class Main extends JavaPlugin implements Plugin, Listener {
 		pacifists.save();
 	}
 
+	@EventHandler
+	public void onWeatherChange(WeatherChangeEvent event) {
+		if (!rain) {
+			event.setCancelled(event.toWeatherState());
+		}
+	}
+
 	@Override
 	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, String label, String[] args) {
 		switch (label) {
@@ -37,8 +47,23 @@ public class Main extends JavaPlugin implements Plugin, Listener {
 			case "pacifist":
 				pacifist(sender, args);
 				break;
+			case "rain":
+				rain(sender, args);
 		}
 		return true;
+	}
+
+	public void rain(CommandSender sender, String[] args) {
+		if(args.length == 0) {
+			sender.sendMessage("You must specify true or false, like /rain false");
+			return;
+		}
+		try {
+			rain = Boolean.parseBoolean(args[0]);
+			Bukkit.broadcastMessage(String.format("Rain is now set to %b", rain));
+		} catch (NumberFormatException e) {
+			sender.sendMessage(String.format("%s is not a valid boolean", args[0]));
+		}
 	}
 
 	public void suicide(CommandSender sender) {
